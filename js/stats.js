a2864a637b18f3811f2da3e61f1bfcbb68a114af
Ines Fonquernie <inesfonquernie@macbook-air-de-ines.home>
2023-12-16 20:54:26 +0100

    debut des statistiques

--- js/stats.js
+++ js/stats.js
@@ -15,7 +15,11 @@ function createBarChart(svgEl, trafficData) {
 
     const yScale = d3.scaleLinear()
                      .range([height, 0])
-                     .domain([0, d3.max(trafficData, d => d.traffic)]);
+                     .domain([0, d3.max(trafficData, d => d.metro + d.rer)]);
+    
+    const colorScale = d3.scaleOrdinal()
+                     .domain(['metro', 'rer'])
+                     .range(["#2171b5", "#9ecae1"]);
 
     const g = svgEl.append("g").attr("transform", `translate(${margin.left},${margin.top})`);
 
@@ -24,21 +28,64 @@ function createBarChart(svgEl, trafficData) {
      .call(d3.axisBottom(xScale));
 
     g.append("g")
-     .call(d3.axisLeft(yScale).tickFormat(d => `${d} passagers`));
+    .call(d3.axisLeft(yScale));
 
-    g.selectAll(".bar")
-     .data(trafficData)
-     .enter().append("rect")
-     .attr("class", "bar")
-     .attr("x", d => xScale(d.year))
-     .attr("width", xScale.bandwidth())
-     .attr("y", d => yScale(0))
-     .attr("height", 0)
-     .transition()
-     .duration(750)
-     .attr("y", d => yScale(d.traffic))
-     .attr("height", d => height - yScale(d.traffic))
-     .attr("fill", "steelblue");
+    g.selectAll(".bar.metro")
+    .data(trafficData)
+    .enter().append("rect")
+    .attr("class", "bar metro")
+    .attr("x", d => xScale(d.year))
+    .attr("y", d => yScale(d.metro))
+    .attr("width", xScale.bandwidth())
+    .attr("height", d => height - yScale(0))
+    .transition()
+    .duration(750)
+    .attr("height", d => height - yScale(d.metro))
+    .attr("fill", colorScale('metro'));
+
+   // Ajout des barres pour RER, empilées sur les barres METRO
+   g.selectAll(".bar.rer")
+    .data(trafficData)
+    .enter().append("rect")
+    .attr("class", "bar rer")
+    .attr("x", d => xScale(d.year))
+    .attr("y", d => yScale(d.metro + d.rer))
+    .attr("width", xScale.bandwidth())
+    .attr("height", d => height - yScale(0))
+    .transition()
+    .duration(750)
+    .attr("height", d => height - yScale(d.rer))
+    .attr("fill", colorScale('rer'));
+
+    // Position de la légende
+    var legend = svgEl.append("g")
+                      .attr("transform", `translate(${width + margin.left + 40}, ${margin.top})`);
+
+    // Couleurs et labels pour la légende
+    var legendData = [
+        { label: "Métro", color: "#2171b5" },
+        { label: "RER", color: "#9ecae1" }
+    ];
+
+    // Créer les éléments de la légende
+    legendData.forEach((d, i) => {
+        var legendRow = legend.append("g")
+                              .attr("transform", `translate(0, ${i * 20})`);
+
+        legendRow.append("rect")
+                 .attr("width", 10)
+                 .attr("height", 10)
+                 .attr("fill", d.color);
+
+        legendRow.append("text")
+                 .attr("x", -10)
+                 .attr("y", 10)
+                 .attr("text-anchor", "end")
+                 .style("text-transform", "capitalize")
+                 .style("font-size", "12px")
+                 .style("fill", "#fff")
+                 .text(d.label);
+    });
 
     svgEl.append("text")
      .attr("x", (width / 2) + 125)
@@ -49,77 +96,79 @@ function createBarChart(svgEl, trafficData) {
      .text('ANNUAL RATP TRAFIC');
 }
 
-function createPieChart(svgEl, data) {
-    var pieWidth = 300,
-        pieHeight = 300,
-        pieMarginTop = 500; // Ajustez cette valeur pour positionner le camembert en dessous du graphique en barres
+function createHorizontalBarChart(svgEl, data) {
+    var barWidth = 300,
+        barHeight = 300, // Hauteur augmentée pour accueillir les barres horizontales
+        barMarginTop = 450;
+
+    // Échelle pour l'axe Y qui représente les stations
+    const yScale = d3.scaleBand()
+                     .rangeRound([0, barHeight])
+                     .padding(0.1)
+                     .domain(data.map(d => d.Station));
+
+    // Échelle pour l'axe X qui représente le trafic
+    const xScale = d3.scaleLinear()
+                     .range([0, barWidth])
+                     .domain([0, d3.max(data, d => d.Trafic)]);
+
+    // Groupe pour le graphique à barres horizontales
+    const gBar = svgEl.append("g").attr("transform", `translate(${200},${barMarginTop})`);
+
+    // Ajouter les axes
+    gBar.append("g")
+        .call(d3.axisLeft(yScale));
     
-    var radius = Math.min(pieWidth, pieHeight) / 2;
-    var gPie = svgEl.append("g").attr("transform", `translate(${pieWidth / 2 + 175},${pieMarginTop + radius})`);
+    gBar.append("g")
+        .attr("transform", `translate(0, ${barHeight})`)
+        .call(d3.axisBottom(xScale).ticks(5));
 
-    var color = d3.scaleOrdinal()
-                  .range(["#041938", "#08306b", "#08519c", "#2171b5", 
-    "#4292c6", "#6baed6", "#9ecae1", "#c6dbef", 
-    "#deebf7", "#f7fbff"])
-                  .domain(data.map(d => d.Station));
+    var blueColors = ["#08306b", "#08519c", "#2171b5", "#4292c6", "#6baed6", 
+        "#9ecae1", "#c6dbef", "#deebf7", "#eff3ff", "#f7fbff"];
 
-    var pie = d3.pie()
-                .sort(null)
-                .value(function(d) { return d.Trafic; });
-
-    var path = d3.arc()
-                 .outerRadius(radius - 10)
-                 .innerRadius(0);
-
-    var labelArc = d3.arc()
-                 .outerRadius(radius)
-                 .innerRadius(radius);
-
-    var arc = gPie.selectAll(".arc")
-               .data(pie(data))
-               .enter().append("g")
-               .attr("class", "arc");
-
-    arc.append("path")
-       .attr("d", path)
-       .attr("fill", function(d) { return color(d.data.Station); });
-
-    // Ensuite, ajoutez les étiquettes de texte
-    arc.append("text")
-       .attr("transform", function(d) {
-           var pos = labelArc.centroid(d);
-           return `translate(${pos})`;
-       })
-       .attr("text-anchor", function(d) {
-           return (d.endAngle + d.startAngle) / 2 > Math.PI ? "end" : "start";
-       })
-       .text(function(d) { return d.data.Station; })
-       .style("font-size", "11px")
-       .style("fill", "#fff")
-       .attr("dy", "0.35em");
+    var color = d3.scaleOrdinal(blueColors).domain(data.map(d => d.Station));
     
+
+    // Créer les barres horizontales
+    gBar.selectAll(".bar")
+        .data(data)
+        .enter().append("rect")
+        .attr("class", "bar")
+        .attr("y", d => yScale(d.Station))
+        .attr("height", yScale.bandwidth())
+        .attr("x", 0)
+        .attr("width", d => xScale(0))
+        .transition()
+        .duration(750)
+        .attr("width", d => xScale(d.Trafic))
+        .attr("fill", (d, i) => color(i)); 
+
+    // Ajouter le titre
     svgEl.append("text")
-       .attr("x", (pieWidth / 2) + 175)
-       .attr("y", 0.9 * pieMarginTop)
-       .attr("text-anchor", "middle")
-       .style("font-size", "20px")
-       .style("fill", "#fff")
-       .text('TOP 10 BUSIEST RATP STATIONS IN 2022');
+        .attr("x", barWidth)
+        .attr("y", barMarginTop - 50)
+        .attr("text-anchor", "middle")
+        .style("font-size", "20px")
+        .style("fill", "#fff")
+        .text('TOP 10 BUSIEST RATP STATIONS IN 2022');
 }
+
+
 
 
 function loadTopStationsData(svgEl){
     d3.dsv(";", "data_ratp/trafic/trafic-annuel-entrant-par-station-du-reseau-ferre-2022.csv", function(d) {
         return { Station: d.Station, Trafic: +d.Trafic };
     }).then(function(data) {
-        // Sort and take the top 10 stations
+        // Trier et prendre les 10 premières stations
         var topStations = data.sort((a, b) => b.Trafic - a.Trafic).slice(0, 10);
-        createPieChart(svgEl, topStations);
+        createHorizontalBarChart(svgEl, topStations);
     }).catch(function (err) {
-        console.log("Error loading data");
-        console.log(err);
+        console.warn("Error loading data", err);
     });
 }
+
+
 
 
 function loadData(svgEl){
@@ -134,11 +183,16 @@ function loadData(svgEl){
         d3.dsv(";", "data_ratp/trafic/trafic-annuel-entrant-par-station-du-reseau-ferre-2021.csv"),
         d3.dsv(";", "data_ratp/trafic/trafic-annuel-entrant-par-station-du-reseau-ferre-2022.csv")
       ]).then(function(files) {
-        let trafficData = [];
-        files.forEach((data, index) => {
-        let year = 2014 + index; 
-        let totalTraffic = d3.sum(data, d => +d.Trafic);
-        trafficData.push({ year: year, traffic: totalTraffic });
+        let trafficData = files.map((data, index) => {
+            let yearData = { year: 2014 + index, metro: 0, rer: 0 };
+            data.forEach(d => {
+                if (d.Réseau === "Métro") {
+                    yearData.metro += +d.Trafic;
+                } else if (d.Réseau === "RER") {
+                    yearData.rer += +d.Trafic;
+                }
+            });
+            return yearData;
         });
         createBarChart(svgEl, trafficData);
         
